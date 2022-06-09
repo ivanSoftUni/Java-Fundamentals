7d5f172415103222bf49e8419b63a988f52462eb
ivanSoftUni <ivan_pz86@mail.bg>
2022-06-09 15:08:19 +0300

    Exercises

---
+++ ArraysMoreEx03/LadyBug.java
@@ -0,0 +1,18 @@
+package ArraysMoreEx03;
+
+import java.util.Scanner;
+
+public class LadyBug {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int[] field = new int[n];
+
+
+        }
+
+
+
+    }
+
